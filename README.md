d6f33e6e2b57d76d43a29956de79fcbe26fd0fe3
Antoine Gagnon <antoine.gagnon760@gmail.com>
2023-02-28 18:49:37 -0500

    Update README

--- README.md
+++ README.md
@@ -1,7 +1,25 @@
 # Voy
 
-Generate training plans for your sport with GPT-3
+A proof of concept web app to gnerate training plans for your sport with AI
 
+## Demo
 
 https://user-images.githubusercontent.com/25696312/222008305-1352467d-5f1c-473d-9038-29cd42eed2cc.mp4
 
+## What AI?
+
+This proof of concept uses OpenAI's GPT-3 model via OpenAI's API.
+
+## Stack
+
+- NextJS
+- Tailwind
+- tRPC
+- Prisma
+- next-auth
+
+_Created with [create-t3-app](https://github.com/t3-oss/create-t3-app)_
+
+## Proof of concept disclaimer
+
+This is a quickly put together proof of concept, the code is scrappy and quickly written, not a baseline for anything.
--- src/utils/openai.ts
+++ src/utils/openai.ts
@@ -22,96 +22,6 @@ export async function generateGoal({
   goal,
   unit,
 }: GenerateGoalParams) {
-  const description =
-    "Natural Language training plan to reach a goal to JSON object. The JSON object should contain a name, a description and a list of milestones to reach the goal";
-
-  const examples = [
-    {
-      input: `Give me a plan to do a total of 5000 km of Cycling from 27 February 2023 to 27 May 2023 in this JSON format: {"name": "goal name here", "description": "goal description here", plan: {"month": {"week": {"milestone": "value here", "focus": "description of focus for the week here in motivational tone"}}}. Make sure the total amount of km sums to 5000 and the intensity is gradual every week."`,
-      response: `
-    {
-      "name": "5000km Cycling Challenge",
-      "description": "Challenge myself to cycle 5000km in a span of three months",
-      "plan":
-      {
-        "February": {
-          "Week 1": {
-            "milestone": "400 km",
-            "focus": "Focus on feeling the rhythm of the road and cycle smoothly."
-          },
-          "Week 2": {
-            "milestone": "400 km",
-            "focus": "Challenge yourself to beat your last week's record by even a small fraction."
-          },
-          "Week 3": {
-            "milestone": "450 km",
-            "focus": "Be brave and try ascending hills and challenge yourself."
-          },
-          "Week 4": {
-            "milestone": "450 km",
-            "focus": "Maintain the newfound intensity and keep the fire burning."
-          }
-        },
-        "March": {
-          "Week 1": {
-            "milestone": "500 km",
-            "focus": "Focus on longer term objectives and stay motivated on the roads."
-          },
-          "Week 2": {
-            "milestone": "500 km",
-            "focus": "Break till maintenance week but make sure you don't lose momentum."
-          },
-          "Week 3": {
-            "milestone": "550 km",
-            "focus": "Maintain your cycling gears so that they don't stand in between your goals."
-          },
-          "Week 4": {
-            "milestone": "550 km",
-            "focus": "Keep pushing yourself and strive to do better."
-          }
-        },
-        "April": {
-          "Week 1": {
-            "milestone": "600 km",
-            "focus": "Take time off rest but use the freshness to cycle with more energy."
-          },
-          "Week 2": {
-            "milestone": "600 km",
-            "focus": "Have regular checkups of your joint pains and make sure you're all set."
-          },
-          "Week 3": {
-            "milestone": "650 km",
-            "focus": "Prepare for the last lap and challenge yourself to go for the kill."
-          },
-          "Week 4": {
-            "milestone": "650 km",
-            "focus": "You are almost there! Dig deep and bring out your best for the end spurt."
-          }
-        },
-        "May": {
-          "Week 1": {
-            "milestone": "700 km",
-            "focus": "Stay motivated and take time with the last steps."
-          },
-          "Week 2": {
-            "milestone": "700 km",
-            "focus": "Don't be afraid to rest and relax as well to keep going."
-          },
-          "Week 3": {
-            "milestone": "750 km",
-            "focus": "Pace yourself to finish this epic cycling trip."
-          },
-         "Week 4": {
-            "milestone": "750 km",
-            "focus": "You can do it! Enjoy the last bits of your journey and achieve success!"
-          }
-        }
-       }
-      }
-    `,
-    },
-  ];
-
   const codeEngine = new CodeEngine(description, examples, "", {
     modelConfig: {
       maxTokens: 2000,
@@ -146,3 +56,93 @@ export async function generateGoal({
 
   return { plan, error: null };
 }
+
+const description =
+  "Natural Language training plan to reach a goal to JSON object. The JSON object should contain a name, a description and a list of milestones to reach the goal";
+
+const examples = [
+  {
+    input: `Give me a plan to do a total of 5000 km of Cycling from 27 February 2023 to 27 May 2023 in this JSON format: {"name": "goal name here", "description": "goal description here", plan: {"month": {"week": {"milestone": "value here", "focus": "description of focus for the week here in motivational tone"}}}. Make sure the total amount of km sums to 5000 and the intensity is gradual every week."`,
+    response: `
+  {
+    "name": "5000km Cycling Challenge",
+    "description": "Challenge myself to cycle 5000km in a span of three months",
+    "plan":
+    {
+      "February": {
+        "Week 1": {
+          "milestone": "400 km",
+          "focus": "Focus on feeling the rhythm of the road and cycle smoothly."
+        },
+        "Week 2": {
+          "milestone": "400 km",
+          "focus": "Challenge yourself to beat your last week's record by even a small fraction."
+        },
+        "Week 3": {
+          "milestone": "450 km",
+          "focus": "Be brave and try ascending hills and challenge yourself."
+        },
+        "Week 4": {
+          "milestone": "450 km",
+          "focus": "Maintain the newfound intensity and keep the fire burning."
+        }
+      },
+      "March": {
+        "Week 1": {
+          "milestone": "500 km",
+          "focus": "Focus on longer term objectives and stay motivated on the roads."
+        },
+        "Week 2": {
+          "milestone": "500 km",
+          "focus": "Break till maintenance week but make sure you don't lose momentum."
+        },
+        "Week 3": {
+          "milestone": "550 km",
+          "focus": "Maintain your cycling gears so that they don't stand in between your goals."
+        },
+        "Week 4": {
+          "milestone": "550 km",
+          "focus": "Keep pushing yourself and strive to do better."
+        }
+      },
+      "April": {
+        "Week 1": {
+          "milestone": "600 km",
+          "focus": "Take time off rest but use the freshness to cycle with more energy."
+        },
+        "Week 2": {
+          "milestone": "600 km",
+          "focus": "Have regular checkups of your joint pains and make sure you're all set."
+        },
+        "Week 3": {
+          "milestone": "650 km",
+          "focus": "Prepare for the last lap and challenge yourself to go for the kill."
+        },
+        "Week 4": {
+          "milestone": "650 km",
+          "focus": "You are almost there! Dig deep and bring out your best for the end spurt."
+        }
+      },
+      "May": {
+        "Week 1": {
+          "milestone": "700 km",
+          "focus": "Stay motivated and take time with the last steps."
+        },
+        "Week 2": {
+          "milestone": "700 km",
+          "focus": "Don't be afraid to rest and relax as well to keep going."
+        },
+        "Week 3": {
+          "milestone": "750 km",
+          "focus": "Pace yourself to finish this epic cycling trip."
+        },
+       "Week 4": {
+          "milestone": "750 km",
+          "focus": "You can do it! Enjoy the last bits of your journey and achieve success!"
+        }
+      }
+     }
+    }
+  `,
+  },
+];
